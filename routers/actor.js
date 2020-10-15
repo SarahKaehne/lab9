6d2298f77801f332280c27259688690d0531b5ed
SarahKaehne <kaehnese@gmail.com>
2020-10-15 17:39:33 +1100

    first commit

---
+++ routers/actor.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+const Actor = require('../models/actor');
+const Movie = require('../models/movie');
+module.exports = {
+    getAll: function (req, res) {
+        if (req.query.min_age == undefined){
+            Actor.find({}).populate('movies').exec(function (err, actors) {
+                if (err) return res.status(404).json(err);
+                res.json(actors);
+            })
+        } else {
+            if (isNaN(req.query.min_age) || isNaN(req.query.max_age)){
+                res.status(404);
+            }
+
+            let miny = moment().subtract(req.query.min_age, 'years').format('YYYY');
+            let maxy = moment().subtract(req.query.max_age, 'years').format('YYYY');
+
+            Actor.find({bYear: {$lte: miny, $gte: maxy}}).populate('movies').exec(function (err, actors){
+                if(err) return res.status(400).json(err);
+                res.json(actors);
+            })
+        }
+    },
+    createOne: function (req, res) {
+        let newActorDetails = req.body;
+        newActorDetails._id = new mongoose.Types.ObjectId();
+        let actor = new Actor(newActorDetails);
+        actor.save(function (err) {
+            res.json(actor);
+        });
+    },
+    getOne: function (req, res) {
+        Actor.findOne({ _id: req.params.id })
+            .populate('movies')
+            .exec(function (err, actor) {
+                if (err) return res.status(400).json(err);
+                if (!actor) return res.status(404).json();
+                res.json(actor);
+            });
+    },
+    updateOne: function (req, res) {
+        Actor.findOneAndUpdate({ _id: req.params.id }, req.body, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+            res.json(actor);
+        });
+    },
+    deleteOne: function (req, res) {
+        Actor.findOneAndRemove({ _id: req.params.id }, function (err) {
+            if (err) return res.status(400).json(err);
+            res.json();
+        });
+    },
+    addMovie: function (req, res) {
+        Actor.findOne({ _id: req.params.id }, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+            Movie.findOne({ _id: req.body.id }, function (err, movie) {
+                if (err) return res.status(400).json(err);
+                if (!movie) return res.status(404).json();
+                actor.movies.push(movie._id);
+                actor.save(function (err) {
+                    if (err) return res.status(500).json(err);
+                    res.json(actor);
+                });
+            })
+        });
+    },
+
+    delActorMovies: function (req, res) {
+        Actor.findOneAndRemove({ _id: req.params.id }, function (err) {
+            if (err) return res.status(400).json(err);
+        });
+        Movie.deleteMany({ actors: req.params.id }, function (err, doc) {
+            if (err) return res.status(400).json(err);
+            res.json();
+        });
+    },
+    delMovieActor:function (req, res) {
+        Actor.findOneAndUpdate({ _id: req.params.aid }, { $unset: { movies: req.params.mid } }, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+            res.json();
+        });
+    }
+};
---
+++ server.js
@@ -0,0 +1,53 @@
+//https://hub.packtpub.com/building-movie-api-express/
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const actors = require('./routers/actor');
+const movies = require('./routers/movie');
+const app = express();
+app.listen(8080);
+app.use(bodyParser.json());
+
+let path = require('path');
+app.use("/", express.static(path.join(__dirname, "dist/movieAng")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.connect('mongodb://localhost:27017/lab7', function (err) {
+    if (err) {
+        return console.log('Mongoose - connection error:', err);
+    }
+    console.log('Connect Successfully');
+});
+//Configuring Endpoints
+//Actor RESTFul endpoionts 
+app.get('/actors', actors.getAll);
+app.post('/actors', actors.createOne);
+app.get('/actors/:id', actors.getOne);
+app.put('/actors/:id', actors.updateOne);
+app.post('/actors/:id/movies', actors.addMovie);
+app.delete('/actors/:id', actors.deleteOne);
+
+//2.Delete an actor and all its movies
+app.delete('/actors/:id/del', actors.delActorMovies);
+//3.Remove a movie from the list of movies of an actor
+app.delete('/actors/:aid/:mid', actors.delMovieActor);
+
+
+//Movie RESTFul  endpoints
+app.get('/movies', movies.getAll);
+app.post('/movies', movies.createOne);
+app.get('/movies/:id', movies.getOne);
+app.put('/movies/:id', movies.updateOne);
+
+//1.Delete a movie by its ID
+app.delete('/movies/:id', movies.deleteOne);
+//4.Remove an actor from the list of actors in a movie
+app.delete('/movies/:mid/:aid/ids', movies.delActorMovie);
+//5.Add an existing actor to the list of actors in a movie
+app.post('/movies/:id/actors', movies.addActor);
+//6.Retrieve (GET) all the movies produced between year1 and year2, where year1>year2
+app.get('/movies/:year1/:year2', movies.movieYear);
+//9.Delete all the movies that are produced between two years.
+app.delete('/delmovies', movies.deleteBetween);
+//lab9 del older
+app.delete('/movies/:year/delOlder', movies.deleteOlderThan);
